80224537f59bc7d883ef45efc12bc63fd0313b92
Oussama taali <oussamataali1997@gmail.com>
2023-03-17 13:38:58 -0700

    ghjhgjh

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,16 @@
  * You are not allowed to use any variable of type char
  * You can only use the putchar function
  * Return: 0
- */
-
+ *
+**/
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-printf("\n");
-return (0);
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
 }
